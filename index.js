42eeb3164e9038e43cdab797f270b969ab42603d
AbeCR <abmwc16@gmail.com>
2024-10-01 20:17:53 -0600

    Practice
    Practice

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-let firstName = 'Mosh', lastName = "Munoz" ;
-console.log(firstName);
+/*
 
-const number = 0.3;
-console.log(number);
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = '3';
+console.log(selectedColors);
+
+
+x = 10;
+y = 5;
+console.log(x+=y); 
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello";
+  }
+    
+
+
+function greet(firstName, lastName) {
+    return ('Hello ' + firstName +" "+ lastName);
+}
+
+console.log(greet('Jhon', 'Martinez'));
+
+*/
+
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(6));
+
 
