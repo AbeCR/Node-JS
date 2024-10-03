baee89676843f6ef198f88812a913433de7bbb36
AbeCR <abmwc16@gmail.com>
2024-10-02 19:36:55 -0600

    Excercies

--- index.js
+++ index.js
@@ -108,13 +108,87 @@
 // }
 // console.log(isLandScape(800, 600));
 
-const output = fizzBuzz("ss");
-console.log(output);
+// const output = fizzBuzz("ss");
+// console.log(output);
 
-function fizzBuzz(input) {
-    if (typeof input !== 'number') return NaN;
-    if ((input % 5 === 0) && (input % 3 === 0)) return "FizzBuzz";
-    if (input % 3 === 0) return "Fizz";
-    if (input % 5 === 0) return "Buzz";
-    return input;
+// function fizzBuzz(input) {
+//     if (typeof input !== 'number') return NaN;
+//     if ((input % 5 === 0) && (input % 3 === 0)) return "FizzBuzz";
+//     if (input % 3 === 0) return "Fizz";
+//     if (input % 5 === 0) return "Buzz";
+//     return input;
+// }
+
+//Speed Limit = 70
+// 5 -> 1 point
+// // 12 points -> License Suspended
+// function checkSpeed(speed){
+//     const speedLimit = 70;
+//     const kmPerPoint = 5;
+
+//     if (speed < speedLimit + kmPerPoint) {
+//         console.log('OK');
+//         return;
+//     }
+//     const points = Math.floor((speed - speedLimit) / kmPerPoint);
+//     if (points >= 12) console.log('License suspended'); 
+//     else console.log(points, 'Points');
+
+// }
+
+// checkSpeed(73);
+
+// function showNumbers(limit) {
+//     for (let i = 0; i <= limit; i++)
+//         if (i % 2 != 0) console.log(i, "ODD");
+//         else console.log(i,"EVEN");
+// }
+
+// function showNumbers(limit) {
+//     for (let i = 0; i <= limit; i++){
+//         const message = (i % 2 != 0) ? 'ODD' : 'EVEN'
+//         console.log(i, message);
+//     }
+// }
+// showNumbers(10);
+
+// const array = [0, null, undefined, '', 2, 3];
+
+// function countTruthy(array){
+//     let count = 0;
+//     for (let value of array){
+//         if (value)
+//             count++;
+//     return count;
+//     }
+// }
+
+// console.log(countTruthy(array));
+
+// const movie = {
+//     title: 'a',
+//     releaseYear: 2018,
+//     rating: 4.5,
+//     director: 'b'
+// };
+
+// function showProperties(obj){  
+//     for (let key in obj){
+//         if (typeof obj[key] === 'string')
+//         console.log(key, obj[key]);
+//     }
+// }
+
+// showProperties(movie);
+
+console.log(sum(10));
+
+function sum(limit) {
+    let sum = 0;
+
+    for (let i = 0; i <= limit; i++)
+        if (i % 3 === 0 || i % 5 === 0)
+            sum += i;
+
+    return sum;
 }
