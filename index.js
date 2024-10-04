bfde2a95ad156a1200a13891d4acb5368f74418f
AbeCR <abmwc16@gmail.com>
2024-10-03 21:19:08 -0600

    Excersices Arrays

--- index.js
+++ index.js
@@ -219,3 +219,65 @@
 //     return true;
 // }
 
+// function showAddress(address) {
+//     for (key in address){
+//         console.log(key, address[key]);
+//     }
+// }
+
+// showAddress(address);
+
+
+
+// function createAddress(street, city, zipCode) {
+//     return {
+//         street,
+//         city,
+//         zipCode
+//     };
+// }
+
+// let address1 = new CreateAddress ('a', 'b', 'c');
+// let address2 = new CreateAddress ('a', 'b', 'c');
+
+// function CreateAddress(street, city, zipCode) {
+//     this.street = street;
+//     this.city = city;
+//     this.zipCode = zipCode
+// }
+
+// function areEqual (address1, address2) {
+//     return (address1.street === address2.street && address1.city === address2.city && address1.zipCode === address2.zipCode);
+// }
+
+// function areSame (address1, address2) {
+//     return (address1 === address2);
+// }
+// console.log(areEqual(address1,address2));
+// console.log(areSame(address1,address2));
+
+
+// let post = new BlogEngine('a','b','c');
+
+// console.log(post);
+
+// function BlogEngine(title, body, author) {
+//     this.title = title;
+//     this.body = body;
+//     this.author = author;
+//     this.views = 0;
+//     this.comments = [];
+//     this.isLive = false
+// }
+
+
+// const numbers = [3,4];
+
+// numbers.push(5,6);
+// numbers.unshift(1,2);
+// numbers.splice(2,0, 'A', 'b');
+
+// console.log(numbers.indexOf('A'));
+// console.log(numbers.includes(1));
+
+
