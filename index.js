787c6e2e7ff5e00ca8d10173281d952edc4d4e4d
AbeCR <abmwc16@gmail.com>
2024-10-06 21:08:49 -0600

    Funct

--- index.js
+++ index.js
@@ -369,16 +369,40 @@
 //     return array.reduce((a,b) => (a>b) ? a : b);
 // }
 
-const movies = [
-    {title: 'a', year: 2018, rating: 4.5 },
-    {title: 'b', year: 2018, rating: 4.5 },
-    {title: 'c', year: 2018, rating: 3 },
-    {title: 'd', year: 2017, rating: 4.5 },
-];
+// const movies = [
+//     {title: 'a', year: 2018, rating: 4.5 },
+//     {title: 'b', year: 2018, rating: 4.5 },
+//     {title: 'c', year: 2018, rating: 3 },
+//     {title: 'd', year: 2017, rating: 4.5 },
+// ];
 
-const titles = movies
-    .filter(m => m.year === 2018 && m.rating >=4)
-    .sort((a, b) => a.rating - b.rating)
-    .reverse()
-    .map(m => m.title)
-console.log(titles);
+// const titles = movies
+//     .filter(m => m.year === 2018 && m.rating >=4)
+//     .sort((a, b) => a.rating - b.rating)
+//     .reverse()
+//     .map(m => m.title)
+// console.log(titles);
+
+/* Functions */
+
+// function walk() {
+//     console.log('walk');
+// }
+
+// let run = function(){
+//     console.log('run');
+// };
+
+// let move = run;
+// if (run===move){
+//     run();
+//     move();
+// }
+
+// function sum (discount, ...prices){
+
+//     const total = prices.reduce((a,b) => a + b);
+//     return total * (1 - discount);
+// }
+
+// console.log(sum(0.1, 20, 30));
